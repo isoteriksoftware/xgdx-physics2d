7f7f5bc8a4c50414a49feb650b6e765278f243d4
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-06-10 23:14:12 +0100

    Update PhysicsManagerTest.java

--- runtime-test/core/src/io/github/isoteriktech/xgdx/physics2d/test/PhysicsManagerTest.java
+++ runtime-test/core/src/io/github/isoteriktech/xgdx/physics2d/test/PhysicsManagerTest.java
@@ -1,10 +1,18 @@
 package io.github.isoteriktech.xgdx.physics2d.test;
 
+import com.badlogic.gdx.graphics.Color;
 import io.github.isoteriktech.xgdx.Scene;
 import io.github.isoteriktech.xgdx.physics2d.PhysicsManager2d;
+import io.github.isoteriktech.xgdx.physics2d.utils.Box2dUtil;
 
 public class PhysicsManagerTest extends Scene {
     public PhysicsManagerTest() {
+        setBackgroundColor(Color.BLACK);
+
         PhysicsManager2d physicsManager2d = PhysicsManager2d.setup(this);
+        Box2dUtil.createBoundaryBox(physicsManager2d.getPhysicsWorld(), gameWorldUnits.getWorldWidth(),
+                gameWorldUnits.getWorldHeight(), .2f);
+
+        physicsManager2d.setRenderPhysicsDebugLines(true);
     }
 }
